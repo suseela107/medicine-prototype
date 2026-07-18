6ac31b4cd1d4e215a2299d559f9456937c57d600
suseela107 <susilamadasu45@gmail.com>
2026-07-18 08:32:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,64 @@
-function speakText(){
+const medicineInfo = {
+    en: {
+        name: "Paracetamol 500 mg",
+        uses: "Reduces fever and pain.",
+        dosage: "Take one tablet after food.",
+        sideEffects: "Nausea, dizziness.",
+        expiry: "09-Jan-2028",
+        manufacturer: "ABC Pharma"
+    },
 
-let text=document.getElementById("info").innerText;
+    te: {
+        name: "పారాసిటమాల్ 500 మి.గ్రా",
+        uses: "జ్వరం మరియు నొప్పిని తగ్గిస్తుంది.",
+        dosage: "భోజనం తర్వాత ఒక మాత్ర తీసుకోండి.",
+        sideEffects: "వికారం, తల తిరగడం.",
+        expiry: "09-Jan-2028",
+        manufacturer: "ABC ఫార్మా"
+    },
 
-let language=document.getElementById("language").value;
+    hi: {
+        name: "पैरासिटामोल 500 मि.ग्रा",
+        uses: "बुखार और दर्द कम करता है।",
+        dosage: "भोजन के बाद एक गोली लें।",
+        sideEffects: "मतली, चक्कर आना।",
+        expiry: "09-Jan-2028",
+        manufacturer: "ABC Pharma"
+    },
 
-let speech=new SpeechSynthesisUtterance(text);
+    ta: {
+        name: "பாராசிட்டமால் 500 மி.கி.",
+        uses: "காய்ச்சல் மற்றும் வலியை குறைக்கிறது.",
+        dosage: "உணவுக்குப் பிறகு ஒரு மாத்திரை எடுத்துக்கொள்ளவும்.",
+        sideEffects: "குமட்டல், தலைச்சுற்றல்.",
+        expiry: "09-Jan-2028",
+        manufacturer: "ABC Pharma"
+    }
+};
 
-if(language=="te")
-speech.lang="te-IN";
+function translateText() {
 
-else if(language=="hi")
-speech.lang="hi-IN";
+    const lang = document.getElementById("language").value;
 
-else if(language=="ta")
-speech.lang="ta-IN";
+    document.querySelector("h2").innerText = medicineInfo[lang].name;
 
-else
-speech.lang="en-US";
+    document.getElementById("info").innerHTML = `
+        <b>Uses:</b> ${medicineInfo[lang].uses}<br><br>
+        <b>Dosage:</b> ${medicineInfo[lang].dosage}<br><br>
+        <b>Side Effects:</b> ${medicineInfo[lang].sideEffects}<br><br>
+        <b>Expiry Date:</b> ${medicineInfo[lang].expiry}<br><br>
+        <b>Manufacturer:</b> ${medicineInfo[lang].manufacturer}
+    `;
+}
 
-window.speechSynthesis.speak(speech);
+function speakText() {
+
+    let text = document.getElementById("info").innerText;
+
+    let speech = new SpeechSynthesisUtterance(text);
+
+    window.speechSynthesis.cancel();
+
+    window.speechSynthesis.speak(speech);
 
 }
